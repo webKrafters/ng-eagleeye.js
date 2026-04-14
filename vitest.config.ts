e417e78095345e0b3ec8998f8958ff4f2f852a98
Stephen Isienyi <stephen.isienyi@gmail.com>
2026-04-14 11:45:12 -0700

    Update vitest.config.ts
    Signed-off-by: Stephen Isienyi <stephen.isienyi@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       codeCoverage: true,
       enabled: true,
 			provider: 'istanbul', // or 'v8' or 'custom',
-			reporter: [ 'text', 'json', 'html', 'lcov' ],
+			reporter: [ 'text', 'lcov' ],
       reportsDirectory: './coverage',
     }
 	}
