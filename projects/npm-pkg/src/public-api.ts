954c5e3bf22106dd9b7d8ba207b975b3295a01f2
Stephen Isienyi <stephen.isienyi@gmail.com>
2026-08-30 16:02:53 -0700

    Update public-api.ts
    Signed-off-by: Stephen Isienyi <stephen.isienyi@gmail.com>

--- projects/npm-pkg/src/public-api.ts
+++ projects/npm-pkg/src/public-api.ts
@@ -3,15 +3,19 @@
  */
 export * from './lib';
 export {
+  ChannelPool,
 	CONTEXT_DESCRIPTOR,
+  type ContextData,
 	type ContextServiceConfig,
 	ContextService,
 	provideContextService
 } from './lib/context-service';
 export {
+  BrowserStreamService,
 	type DataSignals,
+  MemoryStreamService,
 	STREAM_DESCRIPTOR,
-	type StreamServiceConfig,
 	StreamService,
+	type StreamServiceConfig,
 	provideStreamService
 } from './lib/stream-service';
